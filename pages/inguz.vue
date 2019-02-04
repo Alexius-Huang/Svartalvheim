a5ca65959de7771d693619627dc5d63874100b50
Maxwell Alexius <destinydragon85614@gmail.com>
2019-02-04 14:33:50 +0800

    Add a middle layer for the Inguz page

--- pages/inguz.vue
+++ pages/inguz.vue
@@ -1,30 +1,148 @@
 <template>
   <main>
-    <line-chart class="chart-section line-chart" />
+    <section class="left">
+      <div class="title-wrapper">
+        <h1 class="title">Visualizations</h1>
+      </div>
+    </section>
+
+    <section class="chart-grid">
+      <div class="chart" @click="$router.push({ name: 'inguz-chart', params: { chart: 'line-chart' } })">
+        <img class="chart-bg" :src="imgs.lineChart" alt="Line Chart" />
+        <span class="label">Line Chart</span>
+      </div>
+    </section>
+
+    <nuxt-child />
   </main>
 </template>
 
 <script>
-import LineChart from '@/components/misc/Inguz/LineChart';
+import lineChartImg from '@/assets/inguz/line-chart-bg.gif';
 
 export default {
-  components: { LineChart },
+  data() {
+    return {
+      imgs: {
+        lineChart: lineChartImg,
+      },
+    };
+  },
 };
 </script>
 
 <style scoped lang="sass">
-main
-  max-width: 960px
-  width: 100vw
-  padding: 48pt 0
-  margin: 0 auto
-  > .chart-section
-    + .chart-section
-      margin-top: 48pt
+@import '../sass/colors.sass'
+@import '../sass/helpers.sass'
+@import '../sass/shared.sass'
 
-@media screen and (max-width: 768px)
+main
+  position: relative
+  width: 100vw
+  font-size: 0
+
+  > section.left
+    position: absolute
+    left: 0
+    top: 0
+    font-size: 0
+    width: calc(100% - 720px)
+    height: 100vh
+    padding: 24pt 24pt 0 24pt
+    box-sizing: border-box
+    overflow-y: auto
+    @include title-style
+
+  > section.chart-grid
+    position: absolute
+    right: 0
+    top: 0
+    width: 720px
+    height: 100vh
+    font-size: 0
+    overflow-y: auto
+    > .chart
+      cursor: pointer
+      font-size: 0
+      vertical-align: top
+      display: inline-block
+      width: 359px
+      height: 360px
+      box-sizing: border-box
+      position: relative
+      overflow: hidden
+
+      &:after
+        content: ''
+        position: absolute
+        width: 100%
+        height: 100%
+        left: 0
+        top: 0
+        opacity: 0
+        background-image: linear-gradient(to top, transparentize($yellow-500, .88) 0%, transparent 33.3%)
+        transition: .25s
+
+      > img.chart-bg
+        width: 100%
+        height: 100%
+        object-fit: cover
+        opacity: .72
+        transition: .25s
+      > span.label
+        position: absolute
+        display: inline-block
+        font-family: $base-font-family
+        font-size: 24pt
+        color: white
+        right: -100%
+        bottom: 10pt
+        transition: .25s
+      &:hover
+        > img.chart-bg
+          opacity: .48
+          transition: .25s
+        > span.label
+          right: 10pt
+          transition: .25s
+      &:hover:after
+        opacity: 1
+        transition: .25s
+
+  > .chart-section
+    position: fixed
+
+@media screen and (max-width: 1080px)
   main
     width: 100vw
-    padding: 24pt
-    box-sizing: border-box
+    > section.left
+      position: relative
+      width: 100%
+      height: auto
+      display: inline-block
+    > section.chart-grid
+      position: relative
+      display: inline-block
+      width: 100%
+      height: auto
+      margin-top: 48pt
+
+      > .chart
+        > img.chart-bg
+          opacity: .48
+          transition: .25s
+        > span.label
+          right: 10pt
+          transition: .25s
+        &:after
+          opacity: 1
+          transition: .25s
+
+
+
+// @media screen and (max-width: 768px)
+//   main
+//     width: 100vw
+//     // padding: 24pt
+//     box-sizing: border-box
 </style>
---
+++ pages/inguz/_chart.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="chart-wrapper" :class="{ hide: !chartType }">
+    <div class="inner-wrapper">
+      <line-chart v-if="chartType === 'line-chart'" />
+    </div>
+  </section>
+</template>
+
+<script>
+import LineChart from '@/components/misc/Inguz/LineChart';
+
+export default {
+  components: { LineChart },
+  computed: {
+    chartType() { return this.$route.params.chart },
+  },
+};
+</script>
+
+<style scoped lang="sass">
+@import '../../sass/helpers.sass'
+@import '../../sass/colors.sass'
+@import '../../sass/shared.sass'
+
+section.chart-wrapper
+  background-color: $grey-900
+  position: fixed
+  left: 0
+  right: 0
+  top: 0
+  width: 100vw
+  height: 100vh
+  overflow: auto
+  margin: 0 auto
+  font-size: 0
+  opacity: 1
+  transition: .25s
+  &.hide
+    opacity: 0
+    pointer-events: none
+    transition: .25s
+  > div.inner-wrapper
+    padding: 48pt 0
+    width: 960px
+    margin: 0 auto
+
+@media screen and (max-width: 960px)
+  section.chart-wrapper
+    width: 100vw
+
+    > div.inner-wrapper
+      width: 100vw
+      padding: 48pt 24pt
+      box-sizing: border-box
+</style>
