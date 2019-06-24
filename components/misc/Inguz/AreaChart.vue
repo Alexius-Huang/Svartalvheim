dd06725bb88cfefadac71f9088c3b726647d52d9
Maxwell Alexius <destinydragon85614@gmail.com>
2019-06-25 00:30:09 +0800

    Inguz - Complete the area chart

--- components/misc/Inguz/AreaChart.vue
+++ components/misc/Inguz/AreaChart.vue
@@ -129,32 +129,46 @@
           </svg>
         </div>
 
-        <div class="btn-group">
+        <div class="label-group">
           <h3 class="title">Visualization Config.</h3>
 
-          <span class="label-group-title">Product Type</span>
-          <template v-for="product in products">
-            <input
-              :disabled="loading"
-              :key="`${product}-toggle-input`" :id="`${product}-toggler`"
-              type="checkbox" v-model="focusedProducts" :value="product"
-            />
-            <label :for="`${product}-toggler`" :key="`${product}-label`">
-              <span class="text">{{ product }}</span>
-              <span class="order">{{ focusedProducts.indexOf(product) + 1 }}</span>
-            </label>
-          </template>
+          <div class="sub-label-group">
+            <h4 class="label-group-title">Area Chart Kind</h4>
+            <input id="standard-chart-mode" type="radio" v-model="chartType" value="Standard" />
+            <label for="standard-chart-mode"><span class="text">Standard</span></label>
 
-          <span class="label-group-title">Time Interval</span>
-          <input id="time-annual-mode" type="radio" v-model="timeIntervalMode" value="Annual" />
-          <label for="time-annual-mode">
-            <span class="text">Annual</span>
-          </label>
+            <input id="stacked-chart-mode" type="radio" v-model="chartType" value="Stacked" />
+            <label for="stacked-chart-mode"><span class="text">Stacked</span></label>
 
-          <input id="time-quarter-mode" type="radio" v-model="timeIntervalMode" value="Quarter" />
-          <label for="time-quarter-mode">
-            <span class="text">Quarter</span>
-          </label>
+            <input id="percentage-chart-mode" type="radio" v-model="chartType" value="Percentage" />
+            <label for="percentage-chart-mode"><span class="text">Percentage</span></label>
+          </div>
+
+          <div class="sub-label-group">
+            <h4 class="label-group-title">Time Interval</h4>
+            <input id="time-annual-mode" type="radio" v-model="timeIntervalMode" value="Annual" />
+            <label for="time-annual-mode"><span class="text">Annual</span></label>
+
+            <input id="time-quarter-mode" type="radio" v-model="timeIntervalMode" value="Quarter" />
+            <label for="time-quarter-mode"><span class="text">Quarter</span></label>
+          </div>
+
+          <br />
+
+          <div class="sub-label-group">
+            <h4 class="label-group-title">Product Type</h4>
+            <template v-for="product in products">
+              <input
+                :disabled="loading"
+                :key="`${product}-toggle-input`" :id="`${product}-toggler`"
+                type="checkbox" v-model="focusedProducts" :value="product"
+              />
+              <label :for="`${product}-toggler`" :key="`${product}-label`">
+                <span class="text">{{ product }}</span>
+                <span class="order">{{ focusedProducts.indexOf(product) + 1 }}</span>
+              </label>
+            </template>
+          </div>
         </div>
       </div>
     </article>
@@ -195,6 +209,21 @@ export default {
           })),
       }), {});
 
+    /* Total Revenue Each Quarter - a map between year-quarters and total revenue */
+    // const TREQ = new Map(Array.from(Array(totalYears))
+    //   .map((_, i) => ([shownFrom + i, [0, 0, 0, 0]])));
+    // products.forEach((product) => {
+    //   const productREQ = PREQ[`${_decapitalize(product)}REQ`];
+    //   productREQ.forEach(({ year, result }) => {
+    //     const cumulatedTotal = TREQ.get(year);
+    //     for (let j = 0; j < 4; j += 1) {
+    //       cumulatedTotal[j] = _round(cumulatedTotal[j] + result[j]);
+    //     }
+
+    //     TREQ.set(year, cumulatedTotal);
+    //   });
+    // });
+
     /* Product Revenue Each Year */
     const PREY = products.reduce((_prey, product) =>
       Object.assign(_prey, {
@@ -203,6 +232,16 @@ export default {
             ({ year, result }) => ({ year, result: _sum(result) })
           )
       }), {});
+
+    /* Total Revenue Each Year - a map between year and total revenue */
+    // const TREY = new Map(Array.from(Array(totalYears))
+    //   .map((_, i) => ([shownFrom + i, 0])));
+    // products.forEach((product) => {
+    //   const productREY = PREY[`${_decapitalize(product)}REY`];
+    //   productREY.forEach(({ year, result }) => {
+    //     TREY.set(year, _round(TREY.get(year) + result));
+    //   });
+    // });
 
     /* Nulled point represent pre-animated state */
     const nulledPoints = [];
@@ -221,15 +260,17 @@ export default {
       chartInfo,
       products,
       data,
-
       shownFrom,
       shownTo,
       totalYears,
       totalQuarters,
 
+      /* UI configurable options */
       focusedProducts: [],
-      timeIntervalMode: 'Annual',   // ['Annual' || 'Quarter']
+      timeIntervalMode: 'Quarter',   // ['Annual' || 'Quarter']
+      chartType: 'Stacked',         // ['Standard' || 'Stacked' || 'Percentage']
 
+      /* Chart rendering data info */
       svg: { width: 960, height: 600 },
       axis: { ...chartSize, translation: [100, 100] },
       area: { ...chartSize, translation: [100, 100] },
@@ -237,6 +278,18 @@ export default {
         translation: [0, 100],
         gapFromAxis: [10, 20],
         incomeLabel: {
+          standard: {
+            delta: { annual: 20, quarter: 10 },
+            unit: 'M $',
+          },
+          stacked: {
+            delta: { annual: 30, quarter: 10 },
+            unit: 'M $',
+          },
+          percentage: {
+            delta: { annual: 10, quarter: 10 },
+            unit: '%',
+          },
           delta: { annual: 30, quarter: 10 },  // Million U.S. dollar
           unit: 'M $',
         },
@@ -245,9 +298,7 @@ export default {
         title: 'Apple Inc. Unaudited Revenue Summary Data From 2012 to 2018',
         offsetFromChart: 45,
       },
-
       nulledPoints: nulledPoints,
-
       ...PREQ,
       ...PREY,
 
@@ -259,25 +310,135 @@ export default {
       const { axis: { width: w, height: h } } = this;
       return { x1: 0, y1: h, x2: w, y2: h };
     },
-    areaBoundries() {
-      const {
-        timeIntervalMode: TIM,
-        products,
-        focusedProducts,
-        totalQuarters: TQ,
-        totalYears: TY,
-        area: { width, height },
-        label: { incomeLabel: { delta: { annual, quarter } } },
-        nulledPoints,
-      } = this;
-
-      const delta = TIM === 'Quarter' ? quarter : annual;
-      const cumulationCache = Array.from(Array(nulledPoints.length)).map(_ => 0);
-
+    currentUnitAndDelta() {
+      const { timeIntervalMode: TIM, chartType: CT, label } = this;
+      const typeKey = _decapitalize(CT);
+      const modeKey = _decapitalize(TIM);
+      return {
+        delta: label.incomeLabel[typeKey].delta[modeKey],
+        unit: label.incomeLabel[typeKey.unit],
+      };
+    },
+    sortedProducts() {
+      const { focusedProducts, products } = this;
       const sortedProducts = [...focusedProducts];
       products
         .filter(p => !focusedProducts.includes(p))
         .forEach((product) => { sortedProducts.push(product); });
+      return sortedProducts;
+    },
+
+    TREQ() {
+      const { shownFrom, focusedProducts: products, totalYears } = this;
+
+      const TREQ = new Map(Array.from(Array(totalYears))
+        .map((_, i) => ([shownFrom + i, [0, 0, 0, 0]])));
+      products.forEach((product) => {
+        const productREQ = this[`${_decapitalize(product)}REQ`];
+        productREQ.forEach(({ year, result }) => {
+          const cumulatedTotal = TREQ.get(year);
+          for (let j = 0; j < 4; j += 1) {
+            cumulatedTotal[j] = _round(cumulatedTotal[j] + result[j]);
+          }
+
+          TREQ.set(year, cumulatedTotal);
+        });
+      });
+
+      return TREQ;
+    },
+    TREY() {
+      const { shownFrom, focusedProducts: products, totalYears } = this;
+
+      const TREY = new Map(Array.from(Array(totalYears))
+        .map((_, i) => ([shownFrom + i, 0])));
+      products.forEach((product) => {
+        const productREY = this[`${_decapitalize(product)}REY`];
+        productREY.forEach(({ year, result }) => {
+          TREY.set(year, _round(TREY.get(year) + result));
+        });
+      });
+      return TREY;
+    },
+
+    areaBoundries() {
+      const { chartType: type } = this;
+      if (type === 'Standard') {
+        return this.standardAreaBoundries;
+      } else if (type === 'Stacked') {
+        return this.stackedAreaBoundries;
+      }
+      return this.percentageAreaBoundries;
+    },
+
+    standardAreaBoundries() {
+      const {
+        timeIntervalMode: TIM,
+        products,
+        focusedProducts,
+        sortedProducts,
+        totalYears: TY,
+        totalQuarters: TQ,
+        area: { width, height },
+        currentUnitAndDelta: { delta },
+        nulledPoints,
+      } = this;
+
+      const result = {};
+
+      if (TIM === 'Annual') {
+        sortedProducts.reverse().forEach((product, i) => {
+          let points;
+          if (focusedProducts.includes(product)) {
+            points = this[`${_decapitalize(product)}REY`]
+              .map(({ result }) => result)
+              .flatMap((value, j) => {
+                const y = _round(height * (1 - (value / (10 * delta))));
+                const x = _round((width / (TY - 1)) * j);
+                const point = [x, y];
+                return [point, point, point, point];
+              });
+          } else {
+            points = Array.from(nulledPoints);
+          }
+
+          Object.assign(result, { [product]: points });
+        });
+      } else /* if (TIM === 'Quarter') */ {
+        sortedProducts.reverse().forEach((product, i) => {
+          let points;
+          if (focusedProducts.includes(product)) {
+            points = this[`${_decapitalize(product)}REQ`]
+              .flatMap(({ result }) => result)
+              .map((value, j) => {
+                const y = _round(height * (1 - (value / (10 * delta))));
+                const x = _round((width / (TQ - 1)) * j);
+                return [x, y];
+              });
+          } else {
+            points = Array.from(nulledPoints);
+          }
+
+          Object.assign(result, { [product]: points });
+        });
+      }
+      return result;
+    },
+
+    stackedAreaBoundries() {
+      const {
+        timeIntervalMode: TIM,
+        products,
+        focusedProducts,
+        sortedProducts,
+        totalQuarters: TQ,
+        totalYears: TY,
+        area: { width, height },
+        currentUnitAndDelta: { delta },
+        nulledPoints,
+      } = this;
+
+      const cumulationCache = Array.from(Array(nulledPoints.length)).map(_ => 0);
 
       if (TIM === 'Annual') {
         return sortedProducts.reverse().reduce((boundries, product, i) =>
@@ -287,7 +448,7 @@ export default {
                 .map(({ result }) => result)
                 .flatMap((value, j) => {
                   const cumulatedYDelta = cumulationCache[j];
-                  const currentYDelta = (value / delta) * (height / 10);
+                  const currentYDelta = (value / (10 * delta)) * height;
                   const y = _round(height - (currentYDelta + cumulatedYDelta));
                   const x = _round((width / (TY - 1)) * j);
                   const point = [x, y];
@@ -306,21 +467,89 @@ export default {
               .flatMap(({ result }) => result)
               .map((value, j) => {
                 const cumulatedYDelta = cumulationCache[j];
-                const currentYDelta = (value / delta) * (height / 10);
+                const currentYDelta = (value / (10 * delta)) * height;
                 const y = _round(height - (currentYDelta + cumulatedYDelta));
                 const x = _round((width / (TQ - 1)) * j);
-                const point = [x, y];
 
                 cumulationCache[j] += currentYDelta;
-                return point;
+                return [x, y];
               })
           ) : nulledPoints
         }), {});
     },
 
+    percentageAreaBoundries() {
+      const {
+        timeIntervalMode: TIM,
+        products,
+        focusedProducts,
+        sortedProducts,
+        totalYears: TY,
+        totalQuarters: TQ,
+        area: { width, height },
+        currentUnitAndDelta: { delta },
+        nulledPoints,
+        TREQ,
+        TREY,
+      } = this;
+
+      const result = {};
+      const cumulationCache = Array.from(Array(nulledPoints.length)).map(_ => 0);
+
+      if (TIM === 'Annual') {
+        sortedProducts.reverse().forEach((product, i) => {
+          let points;
+          if (focusedProducts.includes(product)) {
+            points = this[`${_decapitalize(product)}REY`]
+              .flatMap(({ year, result: value }, j) => {
+                const totalValue = TREY.get(year);
+                const percentage = value / totalValue;
+
+                const y = _round(height * (1 - (percentage + cumulationCache[j])));
+                const x = _round((width / (TY - 1)) * j);
+                const point = [x, y];
+
+                cumulationCache[j] += percentage;
+                return [point, point, point, point];
+              });
+          } else {
+            points = Array.from(nulledPoints);
+          }
+
+          Object.assign(result, { [product]: points });
+        });
+      } else /* if (TIM === 'Quarter') */ {
+        sortedProducts.reverse().forEach((product, i) => {
+          let points;
+          if (focusedProducts.includes(product)) {
+            points = this[`${_decapitalize(product)}REQ`]
+              .flatMap(({ year, result }, j) => {
+                const totalResult = TREQ.get(year);
+                const percentages = result.map((value, k) => value / totalResult[k]);
+
+                const points = percentages.map((percentage, k) => {
+                  const quarterCount = j * 4 + k;
+                  const y = _round(height * (1 - (percentage + cumulationCache[quarterCount])));
+                  const x = _round((width / (TQ - 1)) * quarterCount);
+
+                  cumulationCache[quarterCount] += percentage;
+                  return [x, y];
+                });
+                return points;
+              });
+          } else {
+            points = Array.from(nulledPoints);
+          }
+
+          Object.assign(result, { [product]: points });
+        });
+      }
+      return result;
+    },
+
     areaChartVariable() {
-      const { focusedProducts: v1, timeIntervalMode: v2 } = this;
-      return [v1, v2];
+      const { focusedProducts: v1, timeIntervalMode: v2, chartType: v3 } = this;
+      return [v1, v2, v3];
     },
   },
   methods: {
@@ -343,11 +572,14 @@ export default {
       return { x1: 0, y1: y, x2: w, y2: y };
     },
     deriveRevenueLabelFromIndex(i) {
-      const { timeIntervalMode: mode, label } = this;
-      const { incomeLabel: { delta, unit } } = label;
-      const { annual: AD, quarter: QD } = delta;
+      const {
+        timeIntervalMode: mode,
+        chartType: type,
+        label: { incomeLabel },
+      } = this;
 
-      return `${(i - 1) * (mode === 'Quarter' ? QD : AD)}${unit}`;
+      const { delta: { [_decapitalize(mode)]: d }, unit } = incomeLabel[_decapitalize(type)];
+      return `${(i - 1) * d}${unit}`;
     },
     /* Polygon requires the left-bottom and the right bottom corner points in the chart */
     polygonize(points) {
@@ -421,6 +653,8 @@ section.bar-chart
     &:hover
       color: $yellow-300
 
+$product-color-map: ('iPhone': $yellow-700, 'iPad': $light-green-800, 'Mac': $teal-800, 'Other': $blue-900)
+
 svg#area-chart > g.area-visualization-group > g.area-group
   > polyline
     fill: none
@@ -431,26 +665,12 @@ svg#area-chart > g.area-visualization-group > g.area-group
   > polygon
     transition: points .25s
 
-  &.iPhone
-    > polyline
-      stroke: $yellow-700
-    > polygon
-      fill: url(#iPhone-area-gradient)
-  &.iPad
-    > polyline
-      stroke: $light-green-800
-    > polygon
-      fill: url(#iPad-area-gradient)
-  &.Mac
-    > polyline
-      stroke: $teal-800
-    > polygon
-      fill: url(#Mac-area-gradient)
-  &.Other
-    > polyline
-      stroke: $blue-900
-    > polygon
-      fill: url(#Other-area-gradient)
+  @each $product-name, $product-color in $product-color-map
+    &.#{$product-name}
+      > polyline
+        stroke: $product-color
+      > polygon
+        fill: url(##{$product-name}-area-gradient)
 
   &.hide > polygon,
   &.hide > polyline
@@ -479,6 +699,8 @@ svg#area-chart > g.area-visualization-group > g.area-group
 @import '../../../sass/colors.sass'
 @import '../../../sass/shared.sass'
 @import './_shared.sass'
+
+$product-color-map: ('iPhone': $yellow-500, 'iPad': $light-green-500, 'Mac': $teal-500, 'Other': $blue-300)
 
 div.chart.finance-accounting-example
   margin-top: 50pt
@@ -545,25 +767,29 @@ div.chart.finance-accounting-example
         font-size: 10pt
         fill: #aaa
 
-  > div.btn-group
-    margin-top: 24pt
+  > div.label-group
+    margin-top: 12pt
     > h3.title
       font: 14pt $base-font-family
       color: $yellow-500
       margin-bottom: 12pt
+
+  > div.label-group > div.sub-label-group
+    display: inline-block
+    margin-right: 12pt
+    margin-top: 10pt
 
     > input[type="checkbox"],
     > input[type="radio"],
     > label > span.order
       text-align: center
       display: none
-      width: 20pt
-      height: 20pt
-      line-height: 30pt
-      border-radius: 10pt
-      font: 14pt $base-font-family
+      width: 16pt
+      height: 16pt
+      border-radius: 8pt
       background-color: #333
-      margin-left: 5pt
+      margin-left: 2pt
+      font: 12pt/16pt $base-font-family
 
     > label
       @include vertical-align
@@ -573,20 +799,19 @@ div.chart.finance-accounting-example
       color: #888
       box-sizing: border-box
       border: 1pt solid #888
-      border-radius: 1pt
+      border-radius: 2pt
       padding: 0 8pt
       height: 30pt
       transition: .25s
       cursor: pointer
 
-    > span.label-group-title
-      display: inline-block
-      font: bolder 12pt/30pt $default-font-family
-      margin-right: 8pt
+    > h4.label-group-title
+      font: bolder 10pt $default-font-family
       color: white
+      margin-bottom: 5pt
 
-    > label + span.label-group-title
-      margin-left: 12pt
+    > label + h4.label-group-title
+      margin-top: 12pt
 
     > input[type="checkbox"]:disabled,
     > input[type="radio"]:disabled
@@ -601,26 +826,18 @@ div.chart.finance-accounting-example
       + label > span.order
         display: inline-block
 
-      &#iPhone-toggler + label,
       &#time-annual-mode + label,
-      &#time-quarter-mode + label
+      &#time-quarter-mode + label,
+      &#standard-chart-mode + label,
+      &#stacked-chart-mode + label,
+      &#percentage-chart-mode + label,
         border-color: $yellow-500
         background-color: $yellow-500
-        > span.order
-          color: $yellow-500
-      &#iPad-toggler + label
-        border-color: $light-green-500
-        background-color: $light-green-500
-        > span.order
-          color: $light-green-500
-      &#Mac-toggler + label
-        border-color: $teal-500
-        background-color: $teal-500
-        > span.order
-          color: $teal-500
-      &#Other-toggler + label
-        border-color: $blue-300
-        background-color: $blue-300
-        > span.order
-          color: $blue-300
+
+      @each $product-name, $product-color in $product-color-map
+        &##{$product-name}-toggler + label
+          border-color: $product-color
+          background-color: $product-color
+          > span.order
+            color: $product-color
 </style>
